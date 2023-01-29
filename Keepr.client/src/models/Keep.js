bddc4bd1d032a3a40f4c42f82f5a7fad8f819d6a
Kayla Cammack <kaylacammack@gmail.com>
2023-01-29 14:18:04 -0700

    Started home page

---
+++ Keepr.client/src/models/Keep.js
@@ -0,0 +1,14 @@
+export class Keep {
+    constructor(data) {
+        this.creator = data.creator
+        this.creatorId = data.creatorId
+        this.description = data.description
+        this.keepId = data.id
+        this.keepImg = data.img
+        this.kept = data.kept
+        this.name = data.name
+        this.vaultKeepId = data.vaultKeepId
+        this.views = data.views
+    }
+
+}
